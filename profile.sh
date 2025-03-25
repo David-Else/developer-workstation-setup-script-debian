a7546887c93836ea3db97d821019aeae3ebcb431
David <12832280+David-Else@users.noreply.github.com>
2025-03-25 21:11:23 +0000

    Remove go files from PATH now using system package

--- profile.sh
+++ profile.sh
@@ -1,4 +1,4 @@
-PATH="$HOME/Documents/scripts:$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:$PATH"
+PATH="$HOME/Documents/scripts:$HOME/go/bin:$PATH"
 export PATH
 export NNN_PLUG='i:-!|mediainfo $nnn;d:dragdrop;f:fzcd;p:preview-tui;m:mtpmount;j:autojump'
 export NNN_BMS="d:~/Documents;p:~/Pictures;v:~/Videos;m:~/Music;h:~/;u:/media/$USERNAME;D:~/Downloads;M:${XDG_CONFIG_HOME:-$HOME/.config}/nnn/mounts;a:/run/user/$UID/gvfs"
