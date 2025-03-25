269b660eba1a8349866118d097c0e23644fb1576
David <12832280+David-Else@users.noreply.github.com>
2025-03-25 16:55:52 +0000

    Move .profile to external file

---
+++ profile.sh
@@ -0,0 +1,13 @@
+PATH="$HOME/Documents/scripts:$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:$PATH"
+export PATH
+export NNN_PLUG='i:-!|mediainfo $nnn;d:dragdrop;f:fzcd;p:preview-tui;m:mtpmount;j:autojump'
+export NNN_BMS="d:~/Documents;p:~/Pictures;v:~/Videos;m:~/Music;h:~/;u:/media/$USERNAME;D:~/Downloads;M:${XDG_CONFIG_HOME:-$HOME/.config}/nnn/mounts;a:/run/user/$UID/gvfs"
+export NNN_TRASH=1 # use trash-cli: https://pypi.org/project/trash-cli/
+export NNN_FIFO=/tmp/nnn.fifo
+export NNN_BATTHEME="Visual Studio Dark+"
+export NNN_BATSTYLE="plain"
+export NNN_RCLONE='rclone mount --vfs-cache-mode writes'
+export BAT_THEME="Visual Studio Dark+"
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs -g "!{node_modules,.git,.wine}"'
+export EDITOR="hx"
+export SUDO_EDITOR="$HOME/.cargo/bin/hx"
