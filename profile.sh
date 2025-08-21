763e0e1c46e8a97dbb7b60eb411e383dd6b8e754
David <12832280+David-Else@users.noreply.github.com>
2025-08-21 13:12:34 +0100

    Drop dragdrop plugin as it never worked

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 PATH="$HOME/Documents/scripts:$HOME/go/bin:$PATH"
 export PATH
-export NNN_PLUG='i:-!|mediainfo $nnn;d:dragdrop;f:fzcd;p:preview-tui;m:mtpmount;j:autojump'
+export NNN_PLUG='i:-!|mediainfo $nnn;f:fzcd;p:preview-tui;m:mtpmount;j:autojump'
 export NNN_BMS="d:~/Documents;p:~/Pictures;v:~/Videos;m:~/Music;h:~/;u:/media/$USERNAME;D:~/Downloads;M:${XDG_CONFIG_HOME:-$HOME/.config}/nnn/mounts;a:/run/user/$UID/gvfs"
 export NNN_TRASH=1 # use trash-cli: https://pypi.org/project/trash-cli/
 export NNN_FIFO=/tmp/nnn.fifo
